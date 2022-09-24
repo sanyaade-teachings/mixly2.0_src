4445e6d77e6d9914354cb6f19cd1966c0c2732df
王立帮 <3294713004@qq.com>
2022-09-24 21:46:10 +0800

    Update python_skulpt_car

--- board/python_skulpt_car/blocks/game.js
+++ board/python_skulpt_car/blocks/game.js
@@ -692,6 +692,10 @@ Blockly.Blocks['isCirculationRight'] = {
       data: '移动到距离当前位置最近的岔路口'
     }, {
       data: '输出没有符合条件的路线'
+    }, {
+      data: '将当前位置加入路线'
+    }, {
+      data: '将当前位置移出路线'
     }
   ];
 

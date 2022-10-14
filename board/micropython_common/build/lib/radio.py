3ce0bbbe2f79b9284826fd3ad8d483190a18df51
大汉子民 <48112196+dahanzimin@users.noreply.github.com>
2022-10-14 12:56:44 +0800

    MP ESPNOW ADD txpower

--- board/micropython_common/build/lib/radio.py
+++ board/micropython_common/build/lib/radio.py
@@ -14,12 +14,12 @@ from ubinascii import hexlify,unhexlify
 import network
 
 class ESPNow(espnow.ESPNow):
-    def __init__(self,channel=0):
+    def __init__(self,channel=0,txpower=20):
         super().__init__()
         self.active(True)
         self._channel=channel
         self._nic = network.WLAN(network.AP_IF)
-        self._nic.config(hidden=True,channel=self._channel)
+        self._nic.config(hidden=True,channel=self._channel,txpower=txpower)
         self._nic.active(True)
         
     def send(self,peer,msg):
@@ -52,9 +52,9 @@ class ESPNow(espnow.ESPNow):
         else :
             return None,None
 
-    def set_channel(self,channel):
+    def set_channel(self,channel,txpower=20):
         self._channel = channel   
-        self._nic.config(hidden=True,channel=self._channel)        
+        self._nic.config(hidden=True,channel=self._channel,txpower=txpower)        
 
     def _cb_handle(self, event_code,data):
         '''Callback processing conversion'''

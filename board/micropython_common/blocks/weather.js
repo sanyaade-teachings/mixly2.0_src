3d3a95b3143eb9c8c8f5ec2d7b7f400a4a59bdaa
Justin <bnumht@outlook.com>
2022-11-24 14:26:18 +0800

    update weather

---
+++ board/micropython_common/blocks/weather.js
@@ -0,0 +1,67 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.weather');
+goog.require('Blockly.Blocks');
+
+Blockly.Blocks.weather.HUE = '#27b6ac';
+
+Blockly.Blocks['WEATHER_NOW']={
+    init: function() {
+        this.setColour(Blockly.Blocks.weather.HUE);
+        this.appendDummyInput()
+            .appendField(MSG.catweather)
+            .appendField(new Blockly.FieldDropdown([
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_WEATHER_NOW, "weather_now"], 
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_WEATHER_ALARM, "weather_alarm"], 
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_AIR_NOW, "air_now"],
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_TIDE_DAILY, "tide_daily"],
+                [Blockly.MIXLY_WEB_PLACE+Blockly.Msg.HTML_SEARCH, "location_search"]
+                ]), "mode");
+        this.appendValueInput('key')
+            .appendField(Blockly.MIXLY_API_PRIVATE_KEY);   
+        this.appendValueInput('addr')
+            .appendField(Blockly.MIXLY_GEOGRAPHIC_LOCATION);        
+        this.setInputsInline(true);
+        this.setOutput(true);
+    }
+};
+
+Blockly.Blocks['WEATHER_DAILY']={
+    init: function() {
+        this.setColour(Blockly.Blocks.weather.HUE);
+        this.appendDummyInput()
+            .appendField(MSG.catweather)
+            .appendField(new Blockly.FieldDropdown([
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_WEATHER_DAILY, "weather_daily"], 
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_LIFE_SUGGESTION, "life_suggestion"], 
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_AIR_DAILY, "air_daily"],
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_GEO_SUN, "geo_sun"],
+                [Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_GEO_MOON, "geo_moon"]
+                ]), "mode");
+        this.appendValueInput('key')
+            .appendField(Blockly.MIXLY_API_PRIVATE_KEY);   
+        this.appendValueInput('addr')
+            .appendField(Blockly.MIXLY_GEOGRAPHIC_LOCATION);     
+        this.appendValueInput('day')
+            .appendField(Blockly.MIXLY_WEB_DAILY);            
+        this.setInputsInline(true);
+        this.setOutput(true);
+    }
+};
+
+Blockly.Blocks['WEATHER_HOUR']={
+    init: function() {
+        this.setColour(Blockly.Blocks.weather.HUE);
+        this.appendDummyInput()
+            .appendField(MSG.catweather)
+            .appendField(Blockly.MIXLY_WEB_DATA_SENIVERSE_GET_WEATHER_HOURS);
+        this.appendValueInput('key')
+            .appendField(Blockly.MIXLY_API_PRIVATE_KEY);   
+        this.appendValueInput('addr')
+            .appendField(Blockly.MIXLY_GEOGRAPHIC_LOCATION);     
+        this.appendValueInput('hour')
+            .appendField(Blockly.MIXLY_WEB_HOURS);            
+        this.setInputsInline(true);
+        this.setOutput(true);
+    }
+};

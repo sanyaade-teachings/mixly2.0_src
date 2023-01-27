88bfb1b1e51512b44a53f624a9b256df8d014b5b
大汉子民 <48112196+dahanzimin@users.noreply.github.com>
2023-01-27 13:41:32 +0800

    MP- Matrix8x5 from hard serial port to soft serial port

--- board/micropython_common/build/lib/tm1652.py
+++ board/micropython_common/build/lib/tm1652.py
@@ -4,24 +4,23 @@ TM1652-framebuf
 Micropython	library for the TM1652 Matrix8x5
 =======================================================
 
-#Preliminary composition	  	    			20220614
-#Use UART transmission to ensure accuracy	  	20220624
+#Preliminary composition	  	    20230126
 
 dahanzimin From the Mixly Team
 """
-
 import time
 import framebuf
-from machine import UART
+from machine import Pin
 from micropython import const
 
 _TM1652_REG_ADD	     = const(0x08)		#Display address command
 _TM1652_REG_CMD	     = const(0x18)		#Display control command
-_TM1652_SET_CUR	     = const(0x08)      #LED current setting 2/8 
+_TM1652_SET_CUR	     = const(0x04)      #LED current setting 3/8 
 
 class TM1652(framebuf.FrameBuffer):
-	def __init__(self, pin, brightness=0.5):
-		self._uart=UART(1, tx=pin, baudrate=19200,bits=8, parity=1, stop=1)
+	def __init__(self, pin, brightness=0.3):
+		self.pin=Pin(pin,Pin.OUT)
+		self.pin.value(1)	
 		self._buffer = bytearray(5)	
 		super().__init__(self._buffer, 8, 5, framebuf.MONO_HMSB)	
 		
@@ -31,13 +30,37 @@ class TM1652(framebuf.FrameBuffer):
 		time.sleep_ms(5)
 		self.fill(0)
 		self.show()
-			
+
+	def _write_cmd(self, val):
+		'''Serial write command'''
+		falg=0
+		#Start bit
+		self.pin.value(1)
+		time.sleep_us(15)
+		self.pin.value(0)		
+		time.sleep_us(30)
+		#Data bits	
+		for i in range(8):		
+			if (val >> i) & 0x01:
+				self.pin.value(1)
+				falg+=1
+			else:
+				self.pin.value(0)
+				falg+=0
+			time.sleep_us(45)
+		#Check bit
+		self.pin.value(1) if  falg%2 == 0 else self.pin.value(0)
+		time.sleep_us(50)
+		#Stop bit
+		self.pin.value(1)			
+		time.sleep_us(15)
+
 	def get_brightness(self):
-		return self.brightness
+		return round(self.brightness,2)
 
 	def set_brightness(self, brightness):
 		if not 0.0 <= brightness <= 1.0:
-			raise ValueError("Brightness must be a decimal number in the range: 0.0-1.0")
+			raise ValueError("Brightness must be a decimal number in the range: 0.0~1.0")
 		self.brightness = brightness
 		xbright = round(15 * brightness) 
 		xbright = ((xbright & 0xA) >>1) | ((xbright & 0x5) <<1)
@@ -46,11 +69,14 @@ class TM1652(framebuf.FrameBuffer):
 
 	def show(self):
 		"""Refresh the display and show the changes."""
-		self._uart.write(bytes([_TM1652_REG_ADD])+self._buffer)
-		time.sleep_ms(5)
-		self._uart.write(bytes([_TM1652_REG_CMD,self._brightness]))
-		time.sleep_ms(5)
-		
+		self._write_cmd(_TM1652_REG_ADD)
+		for i in range(5):
+			self._write_cmd(self._buffer[i])
+		time.sleep_ms(3)
+		self._write_cmd(_TM1652_REG_CMD)
+		self._write_cmd(self._brightness)
+		time.sleep_ms(3)
+
 	def set_buffer(self, buffer):
 		for i in range(min(len(buffer),len(self._buffer))):
 			self._buffer[i] = self._buffer[i] | buffer[i]

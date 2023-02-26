b57ee673b5f4a75c5b5a8f6e11f482f5fa0c67f2
王立帮 <3294713004@qq.com>
2023-02-26 20:55:48 +0800

    fix: arduino-esp32xx serialObjName option error

--- board/common/blockly-core/arduino.js
+++ board/common/blockly-core/arduino.js
@@ -77,27 +77,27 @@ Blockly.Generator.prototype.INDENT="  ";
         for (let i of range) {
             const pinInfo = i.split('-');
             switch (pinInfo.length) {
-                case 1:
-                    const pinNumStr = pinInfo[0].toString();
-                    if (!isNaN(pinNumStr)) {
-                        const pinNum = parseInt(pinNumStr);
-                        pinList.push(pinNum);
-                    }
-                    break;
-                case 2:
-                    const pinNumStr0 = pinInfo[0].toString(),
-                    pinNumStr1 = pinInfo[1].toString();
-                    if (!isNaN(pinNumStr0) && !isNaN(pinNumStr1)) {
-                        let pinNum0 = parseInt(pinNumStr0);
-                        let pinNum1 = parseInt(pinNumStr1);
-                        if (pinNum0 < 0 || pinNum1 < 0) break;
-                        if (pinNum0 > pinNum1)
-                            [ pinNum0, pinNum1 ] = [ pinNum1, pinNum0 ];
-                        for (let j = pinNum0; j <= pinNum1; j++)
-                            if (!pinList.includes(j))
-                                pinList.push(j);
-                    }
-                    break;
+            case 1:
+                const pinNumStr = pinInfo[0].toString();
+                if (!isNaN(pinNumStr)) {
+                    const pinNum = parseInt(pinNumStr);
+                    pinList.push(pinNum);
+                }
+                break;
+            case 2:
+                const pinNumStr0 = pinInfo[0].toString(),
+                pinNumStr1 = pinInfo[1].toString();
+                if (!isNaN(pinNumStr0) && !isNaN(pinNumStr1)) {
+                    let pinNum0 = parseInt(pinNumStr0);
+                    let pinNum1 = parseInt(pinNumStr1);
+                    if (pinNum0 < 0 || pinNum1 < 0) break;
+                    if (pinNum0 > pinNum1)
+                        [ pinNum0, pinNum1 ] = [ pinNum1, pinNum0 ];
+                    for (let j = pinNum0; j <= pinNum1; j++)
+                        if (!pinList.includes(j))
+                            pinList.push(j);
+                }
+                break;
             }
         }
         return pinList;

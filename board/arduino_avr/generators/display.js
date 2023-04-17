38721e4b22c80a7b847c3f5c299c82ed737467e3
peien268 <42307946+peien268@users.noreply.github.com>
2023-04-17 20:33:21 +0800

    修复OLED显示内置图标与字体使用错位问题

--- board/arduino_avr/generators/display.js
+++ board/arduino_avr/generators/display.js
@@ -679,7 +679,7 @@ Blockly.Arduino.oled_print = function () {
     var code = "";
     code = NAME + '.setCursor(' + POS_x + ',';
     code += POS_y + "); \n";
-    code += NAME + ".print(" + TEXT + "); \n";
+    code += NAME + ".print(" + TEXT + "); \n" + NAME + ".setFontPosBottom();\n";
     return code;
 };
 
@@ -688,8 +688,7 @@ Blockly.Arduino.oled_set_EN_Font = function () {
     var FONT_NAME = this.getFieldValue('FONT_NAME');
     var FONT_SIZE = this.getFieldValue('FONT_SIZE');
     var FONT_STYLE = this.getFieldValue('FONT_STYLE');
-    //var code = NAME + ".setFont(u8g2_font_" + FONT_NAME + FONT_STYLE + FONT_SIZE + "_tf);\n" + NAME + ".setFontPosTop();\n";
-    var code = NAME + ".setFont(u8g2_font_" + FONT_NAME + FONT_STYLE + FONT_SIZE + "_tf);\n";
+    var code = NAME + ".setFont(u8g2_font_" + FONT_NAME + FONT_STYLE + FONT_SIZE + "_tf);\n" + NAME + ".setFontPosTop();\n";
     return code;
 };
 
@@ -697,8 +696,7 @@ Blockly.Arduino.oled_set_CN_Font = function () {
     var NAME = this.getFieldValue('NAME') || 'u8g2';
     var FONT_NAME = this.getFieldValue('FONT_NAME');
     var FONT_SIZE = this.getFieldValue('FONT_SIZE');
-    //var code = NAME + ".setFont(u8g2_font_" + FONT_SIZE + FONT_NAME + ");\n" + NAME + ".setFontPosTop();\n";
-    var code = NAME + ".setFont(u8g2_font_" + FONT_SIZE + FONT_NAME + ");\n";
+    var code = NAME + ".setFont(u8g2_font_" + FONT_SIZE + FONT_NAME + ");\n" + NAME + ".setFontPosTop();\n";
     return code;
 };
 Blockly.Arduino.oled_set_ZH_TW_Font = function () {

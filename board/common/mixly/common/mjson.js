0ec08d50b4f6490f6ff918162fdd0d1a86ba7d9c
王立帮 <3294713004@qq.com>
2023-05-08 19:01:40 +0800

    Fix: the object was incorrectly assigned

--- board/common/mixly/common/mjson.js
+++ board/common/mixly/common/mjson.js
@@ -6,34 +6,39 @@ goog.provide('Mixly.MJSON');
 const { MJSON } = Mixly;
 
 MJSON.operate = (jsonObj, optFunc) => {
-    let newJsonObj = { ...jsonObj };
     // 循环所有键
-    for (var key in newJsonObj) {
+    for (var key in jsonObj) {
         //如果对象类型为object类型且数组长度大于0 或者 是对象 ，继续递归解析
-        var element = newJsonObj[key];
+        var element = jsonObj[key];
         if (element.length > 0 && typeof (element) == "object" || typeof (element) == "object") {
             let data = MJSON.operate(element, optFunc);
             for (let i in data) {
-                newJsonObj[key][i] = data[i];
+                jsonObj[key][i] = data[i];
             }
         } else { //不是对象或数组、直接输出
             if (typeof (element) === 'string') {
                 try {
-                    newJsonObj[key] = optFunc(newJsonObj[key]);
+                    jsonObj[key] = optFunc(jsonObj[key]);
                 } catch (e) {
                 }
             }
         }
     }
-    return newJsonObj;
+    return jsonObj;
 }
 
 MJSON.decode = (jsonObj) => {
-    return MJSON.operate(jsonObj, decodeURIComponent);
+    // 深度拷贝对象，防止解码或编码时篡改原有对象
+    let newJsonObj = {};
+    $.extend(true, newJsonObj, jsonObj);
+    return MJSON.operate(newJsonObj, decodeURIComponent);
 }
 
 MJSON.encode = (jsonObj) => {
-    return MJSON.operate(jsonObj, encodeURIComponent);;
+    // 深度拷贝对象，防止解码或编码时篡改原有对象
+    let newJsonObj = {};
+    $.extend(true, newJsonObj, jsonObj);
+    return MJSON.operate(newJsonObj, encodeURIComponent);;
 }
 
 MJSON.parse = (jsonStr) => {

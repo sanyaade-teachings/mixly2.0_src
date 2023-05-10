05bbf05d27a4460ac49d2b70b44bf79afb30bd02
王立帮 <3294713004@qq.com>
2023-05-10 19:11:16 +0800

    Update static-server

--- static-server/static-sslserver.js
+++ static-server/static-sslserver.js
@@ -1,85 +1,27 @@
-/**
- * 静态资源服务器
- *  * 图片
- *  * html
- *  * css
- *  * js
- *  * ...文件
+const https = require('https');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
  
-* 其他语言
-    * PHP（后端语言） + Apache（服务器）
-    * Java + Tomcat
-    * .net + IIS
-    * ...
+const SSLStaticServer = {};
 
-* 在Nodejs中实现静态资源服务器
-    * 所需模块
-        * http
-        * fs
-        * url
-        * path
-* 一次http请求的过程
-    * 请求：request   请求对象
-        * 客户端->服务器
-    * 响应：response  响应对象
-        * 服务器 -> 客户端
-        * 
-* Express 但是一般用这个做静态资源管理器
- */
-
-const http = require("https");
-const path = require("path");
-const fs = require("fs");
-const url = require("url");
-const options = {
-  key: fs.readFileSync('./server.key'),
-  cert: fs.readFileSync('./server.crt')
-};
-// 引入自己的模块 路径下查找
-const mime = require("./mime.js");
-
-let SSLStaticServer = {};
-
-let debug = false;
-
-SSLStaticServer.init = () => {
-    SSLStaticServer.obj = http.createServer(options,(req, res) => {
-        // 静态资源服务器根据不同的请求地址响应不同的内容
-
-        // 1、获取访问路径 ，req.url是请求的路径
-        let {
-            pathname
-        } = url.parse(req.url); //解构赋值 可以看下url.parse的返回值 是对象
-        // 2、把路径转换为系统的绝对路径
-        if (pathname === '/') pathname = 'index.html';
-        const dirPath = path.resolve(__dirname, '../');
-        const realpath = path.join(dirPath + '/', pathname);
-        // console.log("realpath=", realpath);
-
-        // 3、获取请求资源文件的后缀 用于编码格式
-        const extname = path.extname(pathname).substring(1); // .html => html 去掉.
-
-        // 4、利用fs模块读取静态资源
-        fs.readFile(realpath, (err, data) => {
-            if (err) {
-                return res.end("404")
-            }
-            // res.writeHead(200, "content-type:text/html;charset=utf-8")
-            // 利用 mime模块 设置不同资源的文件的编码格式
-            res.writeHead(200, "Content-Type:" + mime[extname] + ";charset=utf-8")
-            // res.write(data)
-            res.end(data)
-        })
-    })
-}
-
-SSLStaticServer.run = (port, serverDebug = false) => {
-    debug = serverDebug;
-    SSLStaticServer.init();
-    //开启服务器，设置端口号
-    SSLStaticServer.obj.listen(port, () => {
-        console.log('Static服务器正在运行 [端口 - ' + port + ']...');
-    });
+SSLStaticServer.run = (port) => {
+    const KEY_PATH = path.resolve(__dirname, './certs/server.key');
+    const CRT_PATH = path.resolve(__dirname, './certs/server.crt');
+    const options = {
+        key: fs.readFileSync(KEY_PATH),
+        cert: fs.readFileSync(CRT_PATH)
+    };
+    const app = express();
+    app.use(express.static('./'));
+    const httpsServer = https.createServer(options, app);
+    httpsServer.listen(port);
+    console.log('Static服务器正在运行 [端口 - ' + port + ', https]...');
+    console.log('访问地址：https://127.0.0.1:' + port);
+    SSLStaticServer.server = httpsServer;
+    SSLStaticServer.app = app;
+    SSLStaticServer.port = port;
+    SSLStaticServer.protocol = 'https';
 }
 
 module.exports = SSLStaticServer;

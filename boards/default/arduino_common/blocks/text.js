716e1b5bf690c780604b4c96317433eb67073bc9
王立帮 <3294713004@qq.com>
2023-08-25 03:20:31 +0800

    Update: Arduino文本分类添加URL和Base64编解码 resolved #I7833N

---
+++ boards/default/arduino_common/blocks/text.js
@@ -0,0 +1,25 @@
+(() => {
+
+    goog.require('Blockly');
+
+    Blockly.Blocks.text_base64_url_codec = {
+        init: function () {
+            this.appendValueInput("VALUE")
+                .setCheck(null)
+                .setAlign(Blockly.ALIGN_LEFT)
+                .appendField(new Blockly.FieldDropdown([
+                    ["Base64", "BASE64"],
+                    ["URL", "URL"]
+                ]), "TYPE")
+                .appendField(new Blockly.FieldDropdown([
+                    ["编码", "ENCODE"],
+                    ["解码", "DECODE"]
+                ]), "OPTION");
+            this.setOutput(true, null);
+            this.setColour(Blockly.Msg['TEXTS_HUE']);
+            this.setTooltip("");
+            this.setHelpUrl("");
+        }
+    };
+
+})();

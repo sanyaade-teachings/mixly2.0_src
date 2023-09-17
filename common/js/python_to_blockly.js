5b8aed1020a43807012b26acb1f2c3bf4f06b4c3
王立帮 <3294713004@qq.com>
2023-09-18 02:02:46 +0800

    Fix: 修复XPython下文本转图形时传入参数args异常

--- common/js/python_to_blockly.js
+++ common/js/python_to_blockly.js
@@ -1652,6 +1652,7 @@ PythonToBlocks.KNOWN_MODULES = {
 PythonToBlocks.prototype.KNOWN_FUNCTIONS = ["append", "strip", "rstrip", "lstrip"];
 PythonToBlocks.KNOWN_ATTR_FUNCTIONS = {};
 PythonToBlocks.prototype.CallAttribute = function(func, args, keywords, starargs, kwargs, node) {
+    args = args ?? [];
     var name = this.identifier(func.attr);
     if (func.value._astname == "Name" || func.value._astname == "Attribute") {
         var module = null;

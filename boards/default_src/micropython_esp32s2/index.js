02aa378e1c6fc72afae91d9d2bc136cd6fe1be8e
王立帮 <3294713004@qq.com>
2023-11-30 23:08:56 +0800

    Update: 更新micropython_esp32s2

---
+++ boards/default_src/micropython_esp32s2/index.js
@@ -0,0 +1,166 @@
+import * as Blockly from 'blockly/core';
+import { Profile } from 'mixly';
+
+import Names from '../micropython_common/others/names';
+import Variables from '../micropython_common/others/variables';
+
+import pins from './blocks/esp32_profile';
+
+import * as PythonVariablesBlocks from '../python/blocks/variables';
+import * as PythonControlBlocks from '../python/blocks/control';
+import * as PythonMathBlocks from '../python/blocks/math';
+import * as PythonTextBlocks from '../python/blocks/text';
+import * as PythonListsBlocks from '../python/blocks/lists';
+import * as PythonDictsBlocks from '../python/blocks/dicts';
+import * as PythonLogicBlocks from '../python/blocks/logic';
+import * as PythonStorageBlocks from '../python/blocks/storage';
+import * as PythonProceduresBlocks from '../python/blocks/procedures';
+import * as PythonTupleBlocks from '../python/blocks/tuple';
+import * as PythonSetBlocks from '../python/blocks/set';
+import * as PythonHtmlBlocks from '../python/blocks/html';
+import * as PythonUtilityBlocks from '../python/blocks/utility';
+
+import * as MicroPythonInputBlocks from '../micropython_common/blocks/inout';
+import * as MicroPythonSystemBlocks from '../micropython_common/blocks/system';
+import * as MicroPythonSerialBlocks from '../micropython_common/blocks/serial';
+import * as MicroPythonCommunicateBlocks from '../micropython_common/blocks/communicate';
+import * as MicroPythonIotBlocks from '../micropython_common/blocks/iot';
+import * as MicroPythonWeatherBlocks from '../micropython_common/blocks/weather';
+import * as MicroPythonAISensorBlocks from '../micropython_common/blocks/ai_sensor';
+import * as MicroPythonSensorOnboardBlocks from '../micropython_common/blocks/sensor_onboard';
+import * as MicroPythonSensorExternBlocks from '../micropython_common/blocks/sensor_extern';
+import * as MicroPythonPeG1Blocks from '../micropython_common/blocks/pe_g1';
+import * as MicroPythonNetworkBlocks from '../micropython_common/blocks/network';
+import * as MicroPythonAIBlocks from '../micropython_common/blocks/ai';
+import * as MicroPythonActuatorOnboardBlocks from '../micropython_common/blocks/actuator_onboard';
+import * as MicroPythonActuatorExternBlocks from '../micropython_common/blocks/actuator_extern';
+import * as MicroPythonDisplayOnboardBlocks from '../micropython_common/blocks/display_onboard';
+import * as MicroPythonDisplayExternBlocks from '../micropython_common/blocks/display_extern';
+import * as MicroPythonFactoryBlocks from '../micropython_common/blocks/factory';
+import * as MicroPythonBlynkBlocks from '../micropython_common/blocks/blynk';
+
+import * as CeGoBlocks from './blocks/ce_go';
+import * as InputBlocks from './blocks/inout';
+import * as PinsBlocks from './blocks/pins';
+
+import * as PythonVariablesGenerators from '../python/generators/variables';
+import * as PythonControlGenerators from '../python/generators/control';
+import * as PythonMathGenerators from '../python/generators/math';
+import * as PythonTextGenerators from '../python/generators/text';
+import * as PythonListsGenerators from '../python/generators/lists';
+import * as PythonDictsGenerators from '../python/generators/dicts';
+import * as PythonLogicGenerators from '../python/generators/logic';
+import * as PythonStorageGenerators from '../python/generators/storage';
+import * as PythonProceduresGenerators from '../python/generators/procedures';
+import * as PythonTupleGenerators from '../python/generators/tuple';
+import * as PythonSetGenerators from '../python/generators/set';
+import * as PythonHtmlGenerators from '../python/generators/html';
+import * as PythonUtilityGenerators from '../python/generators/utility';
+
+import * as MicroPythonInputGenerators from '../micropython_common/generators/inout';
+import * as MicroPythonSystemGenerators from '../micropython_common/generators/system';
+import * as MicroPythonSerialGenerators from '../micropython_common/generators/serial';
+import * as MicroPythonCommunicateGenerators from '../micropython_common/generators/communicate';
+import * as MicroPythonIotGenerators from '../micropython_common/generators/iot';
+import * as MicroPythonWeatherGenerators from '../micropython_common/generators/weather';
+import * as MicroPythonAISensorGenerators from '../micropython_common/generators/ai_sensor';
+import * as MicroPythonSensorOnboardGenerators from '../micropython_common/generators/sensor_onboard';
+import * as MicroPythonSensorExternGenerators from '../micropython_common/generators/sensor_extern';
+import * as MicroPythonPeG1Generators from '../micropython_common/generators/pe_g1';
+import * as MicroPythonNetworkGenerators from '../micropython_common/generators/network';
+import * as MicroPythonAIGenerators from '../micropython_common/generators/ai';
+import * as MicroPythonActuatorOnboardGenerators from '../micropython_common/generators/actuator_onboard';
+import * as MicroPythonActuatorExternGenerators from '../micropython_common/generators/actuator_extern';
+import * as MicroPythonDisplayOnboardGenerators from '../micropython_common/generators/display_onboard';
+import * as MicroPythonDisplayExternGenerators from '../micropython_common/generators/display_extern';
+import * as MicroPythonFactoryGenerators from '../micropython_common/generators/factory';
+import * as MicroPythonBlynkGenerators from '../micropython_common/generators/blynk';
+
+import * as CeGoGenerators from './generators/ce_go';
+import * as InputGenerators from './generators/inout';
+import * as PinsGenerators from './generators/pins';
+
+import './css/color_esp32s2_mixgoce.css';
+
+Object.assign(Blockly.Names, Names);
+Object.assign(Blockly.Variables, Variables);
+
+Profile.default = {};
+Object.assign(Profile, pins);
+Object.assign(Profile.default, pins['MixGo CE']);
+
+Object.assign(
+    Blockly.Blocks,
+    PythonVariablesBlocks,
+    PythonControlBlocks,
+    PythonMathBlocks,
+    PythonTextBlocks,
+    PythonListsBlocks,
+    PythonDictsBlocks,
+    PythonLogicBlocks,
+    PythonStorageBlocks,
+    PythonProceduresBlocks,
+    PythonTupleBlocks,
+    PythonSetBlocks,
+    PythonHtmlBlocks,
+    PythonUtilityBlocks,
+    MicroPythonInputBlocks,
+    MicroPythonSystemBlocks,
+    MicroPythonSerialBlocks,
+    MicroPythonCommunicateBlocks,
+    MicroPythonIotBlocks,
+    MicroPythonWeatherBlocks,
+    MicroPythonAISensorBlocks,
+    MicroPythonSensorOnboardBlocks,
+    MicroPythonSensorExternBlocks,
+    MicroPythonPeG1Blocks,
+    MicroPythonNetworkBlocks,
+    MicroPythonAIBlocks,
+    MicroPythonActuatorOnboardBlocks,
+    MicroPythonActuatorExternBlocks,
+    MicroPythonDisplayOnboardBlocks,
+    MicroPythonDisplayExternBlocks,
+    MicroPythonFactoryBlocks,
+    MicroPythonBlynkBlocks,
+    CeGoBlocks,
+    InputBlocks,
+    PinsBlocks
+);
+
+Object.assign(
+    Blockly.Python.forBlock,
+    PythonVariablesGenerators,
+    PythonControlGenerators,
+    PythonMathGenerators,
+    PythonTextGenerators,
+    PythonListsGenerators,
+    PythonDictsGenerators,
+    PythonLogicGenerators,
+    PythonStorageGenerators,
+    PythonProceduresGenerators,
+    PythonTupleGenerators,
+    PythonSetGenerators,
+    PythonHtmlGenerators,
+    PythonUtilityGenerators,
+    MicroPythonInputGenerators,
+    MicroPythonSystemGenerators,
+    MicroPythonSerialGenerators,
+    MicroPythonCommunicateGenerators,
+    MicroPythonIotGenerators,
+    MicroPythonWeatherGenerators,
+    MicroPythonAISensorGenerators,
+    MicroPythonSensorOnboardGenerators,
+    MicroPythonSensorExternGenerators,
+    MicroPythonPeG1Generators,
+    MicroPythonNetworkGenerators,
+    MicroPythonAIGenerators,
+    MicroPythonActuatorOnboardGenerators,
+    MicroPythonActuatorExternGenerators,
+    MicroPythonDisplayOnboardGenerators,
+    MicroPythonDisplayExternGenerators,
+    MicroPythonFactoryGenerators,
+    MicroPythonBlynkGenerators,
+    CeGoGenerators,
+    InputGenerators,
+    PinsGenerators
+);
---
+++ boards/default_src/micropython_esp32s2/origin/build/lib/mixgo_ce.py
@@ -0,0 +1,243 @@
+"""
+MixGo CE-Onboard resources
+
+Micropython    library for the MixGo CE-Onboard resources
+=======================================================
+
+#Preliminary composition                   20220901
+
+dahanzimin From the Mixly Team
+"""
+
+import time,gc
+from machine import Pin,SoftI2C,ADC,PWM,RTC
+from ws2812 import NeoPixel
+from music import MIDI
+
+'''RTC'''
+rtc_clock=RTC()
+
+'''i2c-onboard'''
+onboard_i2c=SoftI2C(scl = Pin(41), sda = Pin(42), freq = 400000)
+
+'''Version judgment'''
+if 0x15 in onboard_i2c.scan():
+    version=1
+elif 0x26 in onboard_i2c.scan():
+    version=0
+else:
+    print("Warning: Mixgo CE board is not detected, which may cause usage errors")
+
+'''Matrix16x8'''
+try :
+    import matrix16x8
+    onboard_matrix = matrix16x8.Matrix(onboard_i2c)
+except Exception as e:
+    print("Warning: Failed to communicate with Matrix16x8 or",e)
+
+'''6-Button'''
+class Button:
+    def __init__(self, pin):
+        self._pin = Pin(pin, Pin.IN, Pin.PULL_UP)
+        self._flag = True
+
+    def get_presses(self, delay = 1):
+        last_time,presses = time.time(), 0
+        while time.time() < last_time + delay:
+            time.sleep(0.05)
+            if self.was_pressed():
+                presses += 1
+        return presses
+
+    def is_pressed(self):
+        return self._pin.value() == False
+
+    def was_pressed(self):
+        if self._pin.value() != self._flag:
+            time.sleep(0.01)
+            self._flag = self._pin.value()
+            if self._flag:
+                return False
+            else:
+                return True
+
+    def irq(self, handler, trigger):
+        self._pin.irq(handler = handler, trigger = trigger)
+
+A1key = Button(14)
+A2key = Button(21)
+A3key = Button(36)
+A4key = Button(37)
+B1key = Button(0)
+B2key = Button(35)
+
+'''4-TouchPad'''
+class Touch_Pad:
+    _pins = [4, 5, 6, 7]
+    __species = {}
+    __first_init = True
+    def __new__(cls, pin, *args, **kwargs):
+        if pin not in cls.__species.keys():
+            cls.__first_init = True
+            cls.__species[pin]=object.__new__(cls)
+        return cls.__species[pin]   
+
+    def __init__(self, pin):
+        if self.__first_init:
+            self.__first_init = False
+            from machine import TouchPad
+            self._pin = TouchPad(Pin(self._pins[pin]))
+        
+    def is_touched(self,value):
+        return self._pin.read() > value
+
+    def raw_value(self):
+        return self._pin.read()
+
+#Touch with function call
+def touched(pin,value=33000):
+    return Touch_Pad(pin).is_touched(value)  if value  else Touch_Pad(pin).raw_value()
+
+'''2-LED'''     #Repair brightness adjustment range 0-100%    
+class LED:
+    def __init__(self, pin):
+        self._pin =PWM(Pin(pin),freq=5000,duty_u16=65535)
+        self.setbrightness(0)
+        
+    def setbrightness(self,val):
+        if not 0 <= val <= 100:
+            raise ValueError("Brightness must be in the range: 0-100%")
+        self._brightness=val
+        self._pin.duty_u16(65535-val*65535//100)
+
+    def getbrightness(self):
+        return self._brightness
+
+    def setonoff(self,val):
+        if(val == -1):
+            self.setbrightness(100)  if self._brightness<50 else self.setbrightness(0) 
+        elif(val == 1):
+            self.setbrightness(100) 
+        elif(val == 0):
+            self.setbrightness(0) 
+            
+    def getonoff(self):
+        return True if self._brightness>0 else False
+
+led1 = LED(33)
+led2 = LED(34)
+
+'''MIC_Sensor'''
+class MICSensor:
+    def __init__(self, pin):
+        self.adc=ADC(Pin(pin))
+        self.adc.atten(ADC.ATTN_11DB) 
+        
+    def read(self):
+        maxloudness = 0
+        for i in range(5):
+            loudness = self.sample()  
+            if loudness > maxloudness:
+                maxloudness = loudness
+        return maxloudness  
+        
+    def sample(self):
+        values = []
+        for i in range(50):
+            val = self.adc.read_u16()
+            values.append(val)
+        return max(values) - min(values)
+
+onboard_sound = MICSensor(13)
+
+'''3-ADCSensor'''
+class ADCSensor:
+    __species = {}
+    __first_init = True
+    def __new__(cls, pin, *args, **kwargs):
+        if pin not in cls.__species.keys():
+            cls.__first_init = True
+            cls.__species[pin]=object.__new__(cls)
+        return cls.__species[pin]
+
+    def __init__(self, pin):
+        if self.__first_init:
+            self.__first_init = False
+            self._adc=ADC(Pin(pin))
+            self._adc.atten(ADC.ATTN_11DB) 
+            self._switch = Pin(39, Pin.OUT)
+ 
+    def read(self):
+        return self._adc.read_u16()
+
+    def switch(self,val):
+        self._switch.value(val)
+
+def get_brightness():
+    return ADCSensor(15).read() 
+
+'''Function definition of different versions'''
+if version==0:
+    '''ACC-Sensor'''
+    try:
+        import msa301
+        onboard_mxc6655xa = msa301.MSA301(onboard_i2c,front=True)
+    except Exception as e:
+        print("Warning: Failed to communicate with MSA301 or",e)
+
+    '''4-RGB''' 
+    if Pin(40, Pin.IN).value():
+        onboard_rgb = NeoPixel(Pin(8), 4)
+    else:
+        onboard_rgb = NeoPixel(Pin(8), 4, ORDER=(0, 1, 2, 3))
+
+    '''1Buzzer-Music'''
+    Pin(40, Pin.OUT).value(1)
+    onboard_music =MIDI(17)
+
+    '''temperature'''
+    def get_temperature():
+        adc_val = ADCSensor(16).read() 
+        return adc_val * 3.9 / 5900
+
+    def infrared_left():   
+        print("Warning: Old version, without this function")    
+
+    def infrared_right():
+        print("Warning: Old version, without this function")   
+
+if version==1:
+    '''ACC-Sensor'''
+    try:
+        import mxc6655xa
+        onboard_mxc6655xa = mxc6655xa.MXC6655XA(onboard_i2c,front=True) 
+
+        '''temperature'''
+        def get_temperature():
+            return onboard_mxc6655xa.temperature()
+
+    except Exception as e:
+        print("Warning: Failed to communicate with MXC6655XA or",e)
+
+    '''4-RGB''' 
+    onboard_rgb = NeoPixel(Pin(8), 4)
+
+    '''1Buzzer-Music'''
+    onboard_music =MIDI(40)
+
+    def infrared_left():
+        ADCSensor(3).switch(1)
+        time.sleep(0.01)
+        adc=ADCSensor(3).read()
+        ADCSensor(3).switch(0)    
+        return adc    
+
+    def infrared_right():
+        ADCSensor(16).switch(1)
+        time.sleep(0.01)
+        adc=ADCSensor(16).read()
+        ADCSensor(16).switch(0)    
+        return adc 
+
+'''Reclaim memory'''
+gc.collect()

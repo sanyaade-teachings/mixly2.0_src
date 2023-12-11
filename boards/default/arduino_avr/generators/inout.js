cdefd96664691b30709d9ea2f95ac1b44fa682ee
王立帮 <3294713004@qq.com>
2023-12-11 21:37:15 +0800

    Fix: 修复Arduino下digitalWrite中管脚号为变量时pinMode位置错误

--- boards/default/arduino_avr/generators/inout.js
+++ boards/default/arduino_avr/generators/inout.js
@@ -25,10 +25,21 @@ Blockly.Arduino.forBlock['inout_digital_write2'] = function () {
     var dropdown_stat = Blockly.Arduino.valueToCode(this, 'STAT', Blockly.Arduino.ORDER_ATOMIC);
     var code = "";
     var board_type = Mixly.JSFuncs.getPlatform();
-    if (Blockly.Arduino.setups_['setup_input_' + dropdown_pin]) {
-        delete Blockly.Arduino.setups_['setup_input_' + dropdown_pin];
+    var isVar = true;
+    for (var pin of profile.default.digital) {
+        if (pin[0] === dropdown_pin) {
+            isVar = false;
+            break;
+        }
     }
-    Blockly.Arduino.setups_['setup_output_' + dropdown_pin] = 'pinMode(' + dropdown_pin + ', OUTPUT);';
+    if (isVar) {
+        code = code + 'pinMode(' + dropdown_pin + ', OUTPUT);\n';
+    } else {
+        if (Blockly.Arduino.setups_['setup_input_' + dropdown_pin]) {
+            delete Blockly.Arduino.setups_['setup_input_' + dropdown_pin];
+        }
+        Blockly.Arduino.setups_['setup_output_' + dropdown_pin] = 'pinMode(' + dropdown_pin + ', OUTPUT);';
+    }
     code += 'digitalWrite(' + dropdown_pin + ',' + dropdown_stat + ');\n'
     return code;
 };

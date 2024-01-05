519beb0099dde094cad87cd5000bf3f37f517615
王立帮 <3294713004@qq.com>
2024-01-05 09:40:09 +0800

    Update: mp下加入UserOPEvents

--- common/modules/mixly-modules/web/burn-upload.js
+++ common/modules/mixly-modules/web/burn-upload.js
@@ -484,12 +484,22 @@ BU.uploadWithAmpy = (port) => {
                     mainStatusBarTab.changeTo(port);
                     const statusBarSerial = mainStatusBarTab.getStatusBarById(port);
                     statusBarSerial.scrollToBottom(port);
+                    if (window.userOpEvents) {
+                        window.userOpEvents.addRecord({
+                            operation: 'upload-success'
+                        });
+                    }
                 })
                 .catch((error) => {
                     console.log(error);
                     layer.close(index);
                     statusBarTerminal.addValue(`[Error] ${error}\n`);
                     statusBarTerminal.addValue(`==${Msg.Lang['上传失败']}==\n`);
+                    if (window.userOpEvents) {
+                        window.userOpEvents.addRecord({
+                            operation: 'upload-error'
+                        });
+                    }
                 })
                 .finally(async () => {
                     portObj.busy = false;

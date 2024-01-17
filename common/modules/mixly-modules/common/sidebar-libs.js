c211b772626de171f7ad421ae02920e0e09ddc12
王立帮 <3294713004@qq.com>
2024-01-17 21:34:44 +0800

    Update: 添加Mixly.SideBarLibs

---
+++ common/modules/mixly-modules/common/sidebar-libs.js
@@ -0,0 +1,19 @@
+goog.loadJs('common', () => {
+
+goog.require('Mixly.Debug');
+goog.require('Mixly.PageBase');
+goog.provide('Mixly.SideBarLibs');
+
+const {
+    Debug,
+    PageBase
+} = Mixly;
+
+
+class SideBarLibs extends PageBase {
+    constructor() {
+        
+    }
+}
+
+});

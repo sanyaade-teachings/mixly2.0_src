f51b3500d4fda7571c668720c23c6edcbf927035
王立帮 <3294713004@qq.com>
2024-04-04 00:50:19 +0800

    Update: 更新Mixly.Electron.WikiGenerator

--- common/modules/mixly-modules/electron/wiki-generator.js
+++ common/modules/mixly-modules/electron/wiki-generator.js
@@ -1,26 +1,25 @@
 goog.loadJs('electron', () => {
 
+goog.require('path');
 goog.require('Blockly');
+goog.require('Mustache');
 goog.require('Mixly.Env');
-goog.require('Mixly.XML');
 goog.require('Mixly.IdGenerator');
 goog.require('Mixly.Electron');
 goog.provide('Mixly.Electron.WikiGenerator');
 
 const {
     Env,
-    XML,
     IdGenerator,
     Electron
 } = Mixly;
 const fs_extra = Mixly.require('fs-extra');
-const path = Mixly.require('path');
-const request = Mixly.require('request');
 const fs = Mixly.require('fs');
 
 class WikiGenerator {
     static {
-        this.WIKI_PAGE = goog.get(path.join(Env.templatePath, 'markdown/wiki-page.md'));
+        this.WIKI_PAGE_FILE = goog.get(path.join(Env.templatePath, 'markdown/wiki-page-file.md'));
+        this.WIKI_PAGE_DIR = goog.get(path.join(Env.templatePath, 'markdown/wiki-page-dir.md'));
     }
 
     #$xml_ = null;
@@ -30,6 +29,7 @@ class WikiGenerator {
         this.#$xml_ = $xml;
         this.#desPath_ = desPath;
         this.workspace = Mixly.Workspace.getMain().getEditorsManager().getActive().getPage('block').getEditor();
+        this.generator = Mixly.Workspace.getMain().getEditorsManager().getActive().getPage('block').generator;
     }
 
     buildTree($nodes) {
@@ -38,38 +38,30 @@ class WikiGenerator {
             let child = {};
             child.id = $nodes[i].getAttribute('id') ?? IdGenerator.generate();
             if ($nodes[i].nodeName == 'CATEGORY') {
+                child.name = $nodes[i].getAttribute('name') ?? child.id;
                 child.children = this.buildTree($($nodes[i]).children());
+                output.push(child);
             } else if ($nodes[i].nodeName == 'BLOCK') {
+                child.name = $nodes[i].getAttribute('type') ?? child.id;
                 child.children = false;
                 child.xml = $nodes[i].outerHTML;
+                output.push(child);
             }
-            output.push(child);
         }
         return output;
     }
 
-    async walkTree(rootPath, nodes) {
-        for (let node of nodes) {
-            if (node.children) {
-                let desPath = path.resolve(rootPath, node.id);
-                fs_extra.ensureDirSync(desPath);
-                await this.walkTree(desPath, node.children);
-            } else {
-                await this.generateImage(node.xml, path.resolve(rootPath, node.id + '.png'));
-            }
-        }
-    }
-
     async generate() {
         let output = this.buildTree(this.#$xml_.children());
-        this.walkTree('./outputBlock/assets/', output);
+        let info = await this.generateWikiPage('./outputBlock/', {
+            title: 'Keyes Easy Plug',
+            order: 1
+        }, output);
+        fs_extra.outputFileSync(path.join('./outputBlock/', 'README.md'), info.md);
     }
 
-    generateImage(xml, desPath) {
+    generateImage(desPath) {
         return new Promise((resolve, reject) => {
-            this.workspace.clear();
-            let xmlNode = Blockly.utils.xml.textToDom(`<xml>${xml}</xml>`);
-            Blockly.Xml.domToWorkspace(xmlNode, this.workspace)
             Blockly.Screenshot.workspaceToSvg_(this.workspace, (datauri) => {
                 const base64 = datauri.replace(/^data:image\/\w+;base64,/, '');
                 const dataBuffer = new Buffer(base64, 'base64');
@@ -80,29 +72,73 @@ class WikiGenerator {
         });
     }
 
-    generateWikiPage(rootPath, parentInfo, nodes) {
-        if (!nodes.length) {
-            return;
-        }
+    async generateWikiPage(rootPath, parentInfo, nodes) {
         let config = {
+            id: parentInfo.id,
             title: parentInfo.title,
-            order: 
+            order: parentInfo.order
         };
-        for (let i in nodes) {
-            if (nodes[i].children) {
-                let desPath = path.resolve(rootPath, node.id);
-                let wiki = XML.render(WikiGenerator.WIKI_PAGE, {
-                    title: nodes[i].id,
-                    order: i,
-                    index: false,
-                    blocks: []
-                });
-                fs_extra.ensureDirSync(desPath);
-                fs_extra.outputFileSync(path.resolve(desPath, 'README.md'), wiki);
-                this.generateWikiPage(desPath, nodes[i], nodes[i].children);
-            } else {
-
+        let blocksNum = 0;
+        for (let node of nodes) {
+            if (!node.children) {
+                blocksNum += 1;
             }
+        }
+        config.index = !!blocksNum;
+        if (blocksNum) {
+            let blocks = [];
+            for (let node of nodes) {
+                if (node.children) {
+                    continue;
+                }
+                this.workspace.clear();
+                let xmlNode = Blockly.utils.xml.textToDom(`<xml>${node.xml}</xml>`);
+                Blockly.Xml.domToWorkspace(xmlNode, this.workspace);
+                let code = this.generator.workspaceToCode(this.workspace) || '';
+                code = code.replace(/(_E[0-9A-F]{1}_[0-9A-F]{2}_[0-9A-F]{2})+/g, function (s) {
+                    try {
+                        return decodeURIComponent(s.replace(/_/g, '%'));
+                    } catch (error) {
+                        return s;
+                    }
+                });
+                await this.generateImage(path.join(rootPath, '../assets', parentInfo.title, node.id + '.png'))
+                blocks.push({
+                    imgPath: `./assets/${parentInfo.title}/${node.id}.png`,
+                    code: code,
+                    type: node.name
+                });
+            }
+            config.blocks = blocks;
+            return {
+                file: true,
+                md: Mustache.render(WikiGenerator.WIKI_PAGE_FILE, config)
+            };
+        } else {
+            let emptyNum = 0;
+            for (let i in nodes) {
+                if (!nodes[i].children) {
+                    emptyNum += 1;
+                    continue;
+                }
+                let desPath = path.join(rootPath, nodes[i].id);
+                let info = await this.generateWikiPage(desPath, {
+                    id: nodes[i].id,
+                    title: nodes[i].name,
+                    order: (i - 0) + 1
+                }, nodes[i].children);
+                if (info.file) {
+                    fs_extra.outputFileSync(desPath + '.md', info.md);
+                } else if (!info.isEmpty) {
+                    fs_extra.outputFileSync(path.join(desPath, 'README.md'), info.md);
+                }
+            }
+            config.blocks = [];
+            return {
+                file: false,
+                isEmpty: emptyNum == nodes.length,
+                md: Mustache.render(WikiGenerator.WIKI_PAGE_DIR, config)
+            };
         }
     }
 }
---
+++ common/template/markdown/wiki-page-file.md
@@ -0,0 +1,25 @@
+---
+title: {{&title}}
+order: {{&order}}
+index: {{&index}}
+---
+{{#blocks}}
+
+#### **块类型\: {{&type}}**
+
+---
+
+::: tabs
+
+@tab 图形化
+
+<img src="{{&imgPath}}" alt="模块" style="zoom:10%;" />
+
+@tab 代码
+
+```arduino
+{{&code}}
+```
+
+:::
+{{/blocks}}
